a0c43ed70750328a4162029c6871adb697ca0868
myanime <ryan.cassels@hotmail.com>
2017-10-28 15:35:14 +0200

    v2.2 Check requests a bit better

--- seek/spiders/seekspider.py
+++ seek/spiders/seekspider.py
@@ -128,7 +128,7 @@ class SeekScraper(scrapy.Spider):
         self.all_job_ids = []
         driver = self.loadchrome()
         time.sleep(1)
-        DAYS = 1
+        DAYS = 2
         salary_ranges = [0, 30000, 40000, 50000, 60000, 70000, 80000, 100000, 120000, 150000, 200000, 999999]
         # salary_ranges = [0, 30000]
         # for i in range(0, 1):
@@ -272,29 +272,36 @@ class SeekScraper(scrapy.Spider):
             print(apiurl)
             print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
             r = requests.get(apiurl)
+            created = False
             if r.status_code == 200:
                 print("#######################################")
                 print("###########SUCCESSFUL REQUEST##########")
                 print("#######################################")
                 jsonresponse = r.json()
-                item = self.get_json(jsonresponse, item)
-            yield item
+                created, item = self.get_json(jsonresponse, item)
+            if created:
+                yield item
+            else:
+                with open('request_error.txt', 'a') as error_file:
+                    error_file.write(time.strftime('%d.%m %H:%M'))
+                    error_file.write(apiurl)
+                    error_file.write('\n')
+
 
     def get_json(self, jsonresponse, item):
-        if jsonresponse['totalCount'] > 0:
+        def level_1(attrname, item, data):
+            try:
+                item[attrname] = data[attrname]
+            except:
+                traceback.print_exc()
+        def level_2(attr1name, attr2name, attr3name, item, data):
+            try:
+                item[attr1name] = data[attr2name][attr3name]
+            except:
+                traceback.print_exc()
+        if jsonresponse['totalCount'] == 1:
             data = jsonresponse['data'][0]
-            try:
-                item['advertiser_id'] = data['advertiser']['id']
-            except:
-                traceback.print_exc()
-            try:
-                item['advertiser_description'] = data['advertiser']['description']
-            except:
-                traceback.print_exc()
-            try:
-                item['suburbWhereValue'] = data['suburbWhereValue']
-            except:
-                traceback.print_exc()
+
             try:
                 postCodeInt = []
                 pcText = data['suburbWhereValue']
@@ -305,60 +312,26 @@ class SeekScraper(scrapy.Spider):
             except:
                 item['postCode'] = ''
             try:
-                item['classification_description'] = data['classification']['description']
-            except:
-                traceback.print_exc()
-            try:
-                item['subClassification_description'] = data['subClassification']['description']
-            except:
-                traceback.print_exc()
-            try:
-                item['logo_ID'] = data['logo']['id']
-            except:
-                traceback.print_exc()
-            try:
-                item['logo_description'] = data['logo']['description']
-            except:
-                traceback.print_exc()
-            try:
-                item['listingDate'] = data['listingDate']
-            except:
-                traceback.print_exc()
-            try:
-                item['id'] = data['id']
-            except:
-                traceback.print_exc()
-            try:
-                item['title'] = data['title']
-            except:
-                traceback.print_exc()
-            try:
-                item['location'] = data['location']
-            except:
-                traceback.print_exc()
-            try:
-                item['locationWhereValue'] = data['locationWhereValue']
-            except:
-                traceback.print_exc()
-            try:
                 item['teaser'] = data['teaser'].rstrip('\n').rstrip("\r").rstrip("\r").rstrip("\r")
             except:
                 traceback.print_exc()
-            try:
-                item['workType'] = data['workType']
-            except:
-                traceback.print_exc()
-            try:
-                item['salary'] = data['salary']
-            except:
-                traceback.print_exc()
-            try:
-                item['areaWhereValue'] = data['areaWhereValue']
-            except:
-                traceback.print_exc()
-            try:
-                item['area'] = data['area']
-            except:
-                traceback.print_exc()
 
-            return item
+            level_2('advertiser_description', 'advertiser', 'description', item, data)
+            level_2('advertiser_id', 'advertiser', 'id', item, data)
+            level_1('area', item, data)
+            level_1('areaWhereValue', item, data)
+            level_2('classification_description', 'classification', 'description', item, data)
+            level_1('id', item, data)
+            level_1('listingDate', item, data)
+            level_1('location', item, data)
+            level_1('locationWhereValue', item, data)
+            level_2('logo_ID', 'logo', 'id', item, data)
+            level_2('logo_description', 'logo', 'description', item, data)
+            level_1('salary', item, data)
+            level_2('subClassification_description', 'subClassification', 'description', item, data)
+            level_1('suburbWhereValue', item, data)
+            level_1('title', item, data)
+            level_1('workType', item, data)
+
+            return True, item
+        return False, item
