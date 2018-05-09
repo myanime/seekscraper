53c69c383302d8edad78e0f3c5e04c737d81b1cb
myanime <ryan.cassels@hotmail.com>
2018-05-09 23:14:37 +0200

    poorman cron

---
+++ poorman_cron.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+echo "Poor Mans Cron"
+sleep 5
+echo "Starting"
+gnome-terminal --command=/home/media/seek/local_seek.sh --display=:0
+echo "Starting in 9hrs"
+sleep 32400
+gnome-terminal --command=/home/media/seek/local_seek.sh --display=:0
+echo "Starting in 24hrs"
+sleep 86400
+gnome-terminal --command=/home/media/seek/local_seek.sh --display=:0
+echo "Starting in 24hrs"
+sleep 86400
+gnome-terminal --command=/home/media/seek/local_seek.sh --display=:0
+echo "Starting in 24hrs"
+sleep 86400
+gnome-terminal --command=/home/media/seek/local_seek.sh --display=:0
+echo "Starting in 24hrs"
+sleep 86400
+gnome-terminal --command=/home/media/seek/local_seek.sh --display=:0
+echo "Starting in 24hrs"
+sleep 86400
+gnome-terminal --command=/home/media/seek/local_seek.sh --display=:0
+echo "Starting in 24hrs"
+sleep 86400
+gnome-terminal --command=/home/media/seek/local_seek.sh --display=:0
+
