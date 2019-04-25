e2b043ecea18f96494bb1dbed89d67fb33dc4e47
myanime <ryan.cassels@hotmail.com>
2019-04-25 23:35:38 +0200

    linux path

--- local_seek.sh
+++ local_seek.sh
@@ -1,7 +1,9 @@
 echo "start:" >> ./runcounter
 date >> ./runcounter
-PROJECT_DIR='/Users/ryan/repos/seekscraper'
-AWS_KEY="/Users/ryan/.ssh/aws_schlupfi.pem"
+#PROJECT_DIR='/Users/ryan/repos/seekscraper'
+PROJECT_DIR='/home/myanime/seekscraper'
+#AWS_KEY="/Users/ryan/.ssh/aws_schlupfi.pem"
+AWS_KEY="/home/myanime/.ssh/aws_schlupfi.pem"
 MYDATE=$(date +"%d_%m_%Y")
 
 cd $PROJECT_DIR/static/
@@ -22,7 +24,7 @@ cd $PROJECT_DIR/static/output
 python deduplicate.py
 sleep 5
 cd $PROJECT_DIR/static/output/transfer
-#gzip *.*
-#cp * /home/myanime/seekscraper/static/output/backup/
-#scp -i $AWS_KEY -r $PROJECT_DIR/static/output/transfer/* ubuntu@52.59.254.43:./countries/seek
-#rm $PROJECT_DIR/static/output/transfer/*
+gzip *.*
+cp * /home/myanime/seekscraper/static/output/backup/
+scp -i $AWS_KEY -r $PROJECT_DIR/static/output/transfer/* ubuntu@52.59.254.43:./countries/seek
+rm $PROJECT_DIR/static/output/transfer/*
